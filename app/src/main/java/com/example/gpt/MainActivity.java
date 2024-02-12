19369f9474e889310a9e60fce9dab9e2c46a85e3
litianjun05090 <lotianjun0509@gmail.com>
2024-02-12 21:45:29 +0800

    流式回答

--- app/src/main/java/com/example/gpt/MainActivity.java
+++ app/src/main/java/com/example/gpt/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.gpt;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -11,6 +12,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import com.example.gpt.ChatManager;
+import com.example.gpt.ChatManager.ChatMessage;
+import com.example.gpt.ChatManager.ChatMessage.ChatRole;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -33,16 +37,19 @@ import okhttp3.ResponseBody;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String API_KEY="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";
     RecyclerView recyclerView;
     TextView welcomeTextView;
     EditText messageEditText;
     ImageButton sendButton;
     List<Message> messageList;
     MessageAdapter messageAdapter;
+    ChatApiClient chatApiClient = null;
+    private ChatManager.MessageList multiChatList = new ChatManager.MessageList();
+    private Handler handler = new Handler();
+    private String chatApiBuffer = "";
 
-    public static final MediaType JSON = MediaType.get("application/json");
 
-    OkHttpClient client = new OkHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,6 +78,38 @@ public class MainActivity extends AppCompatActivity {
             welcomeTextView.setVisibility(View.GONE);
         });
 
+
+        chatApiClient=new ChatApiClient(this, "https://api.openai.com/",
+                API_KEY, "gpt-3.5-turbo", new ChatApiClient.OnReceiveListener() {
+            @Override
+            public void onMsgReceive(String message) {
+                chatApiBuffer+=message;
+                handler.post(()->{
+                    messageList.get(messageList.size()-1).setMessage(chatApiBuffer);
+                    messageAdapter.notifyDataSetChanged();
+                });
+                System.out.println("onMsgReceive");
+            }
+
+            @Override
+            public void onError(String message) {
+
+            }
+
+            @Override
+            public void onFunctionCall(String name, String arg) {
+
+            }
+
+            @Override
+            public void onFinished(boolean completed) {
+                handler.post(()->{
+                    sendButton.setImageResource(R.drawable.baseline_send_24);
+                    sendButton.setEnabled(true);
+                });
+            }
+        });
+
     }
 
     void addToChat(String message, String sendBy) {
@@ -86,104 +125,17 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    void addResponse(String response) {
-
-//            messageList.remove(messageList.size() - 1);
-            addToChat(response, Message.SEND_BY_BOT);
-
-
-    }
 
     void callAPI(String question) {
         //okhttp
+        sendButton.setImageResource(R.drawable.ic_send_disable);
+        sendButton.setEnabled(false);
+
+        chatApiBuffer="";
         messageList.add(new Message("正在回复~~", Message.SEND_BY_BOT));
+        multiChatList.add(new ChatMessage(ChatRole.ASSISTANT).setText(question));
+        chatApiClient.sendPromptList(multiChatList);
 
-        JSONObject jsonBody = new JSONObject();
-
-        try {
-            // 创建messages数组，包含用户角色和问题内容
-            JSONArray messagesArray = new JSONArray();
-            JSONObject systemMessage = new JSONObject();
-            systemMessage.put("role", "system");
-            systemMessage.put("content", "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.");
-            JSONObject userMessage = new JSONObject();
-            userMessage.put("role", "user");
-            userMessage.put("content", question);
-            messagesArray.put(systemMessage);
-            messagesArray.put(userMessage);
-            // 构建完整的JSON请求体
-            jsonBody.put("model", "gpt-3.5-turbo"); // 模型选择
-            jsonBody.put("messages", messagesArray);
-
-        }catch (JSONException e) {
-            throw new RuntimeException(e);
-        }
-
-        RequestBody body = RequestBody.create(JSON, jsonBody.toString());
-        Request request = new Request.Builder()
-                .url("https://api.openai.com/v1/chat/completions") // 服务器地址
-                .header("Authorization", "Bearer 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") // 填写你的key
-                .header("Content-Type", "application/json")
-                .post(body)
-                .build();
-        client.newBuilder().readTimeout(50000, TimeUnit.MILLISECONDS).build().newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                addResponse("响应失败" + e.getMessage());
-                Log.d("TAG", "onFailure: ------" + e.getMessage());
-            }
-//
-            @Override
-            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-
-                if (response.isSuccessful()) {
-                    JSONObject jsonObject = null;
-//                    String responseBody = response.body().string();
-
-                    ResponseBody responseBody = response.body();
-
-                    BufferedReader bufferedReader =new BufferedReader(responseBody.charStream());
-                    String line = bufferedReader.readLine();
-                    int index = 0;
-                    StringBuilder sb =new  StringBuilder();
-                    while (line != null) {
-//                        String msg = convert(line, "1", index++);
-                            sb.append(line);
-                        addResponse(sb.toString().trim());
-                        System.out.println(line);
-                        line = bufferedReader.readLine();
-
-                    }
-
-
-//                    try {
-//                        // 解析服务器响应的JSON数据
-//                        jsonObject = new JSONObject(responseBody);
-//                        Log.d("TAG", "onResponse: ---------" + responseBody);
-//                        // 从JSON中提取需要的信息
-//                        JSONArray choicesArray = jsonObject.getJSONArray("choices");
-//                        JSONObject firstChoice = choicesArray.getJSONObject(0);
-//                        JSONObject message = firstChoice.getJSONObject("message");
-//                        String result = message.getString("content");
-//
-//                        // 将提取的结果添加到聊天列表中
-//                        addResponse(result.trim());
-//                    } catch (JSONException e) {
-//                        throw new RuntimeException(e);
-//                    } finally {
-//                        response.close();
-//                    }
-
-
-
-
-
-                } else {
-                    addResponse("响应失败" + response.body().string());
-                    Log.d("TAG", "onResponse: ---------" + response.body().toString());
-                }
-            }
-        });
     }
 
 
